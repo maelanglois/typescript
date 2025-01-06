ca16b1145c4926e6ccd737fea6b8736ec8462901
maelanglois <150166864+maelanglois@users.noreply.github.com>
2025-01-06 16:38:05 +0100

    first push

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import { CLI } from "./CLI";
+
+const startupParts = [
+  "   __________  ____  ___       ____  ___    _   ____ __",
+  "  / ____/ __ \\/ __ \\/   |     / __ )/   |  / | / / //_/",
+  " / /   / / / / / / / /| |    / __  / /| | /  |/ / ,<   ",
+  "/ /___/ /_/ / /_/ / ___ |   / /_/ / ___ |/ /|  / /| |  ",
+  "\\____/\\____/_____/_/  |_|  /_____/_/  |_/_/ |_/_/ |_|",
+  "",
+  "La banque de demain, aujourd'hui.",
+  "",
+];
+
+console.log(startupParts.join("\n"));
+
+// TODO
+
+const cli = new CLI([
+  {
+    title: "Créer un compte",
+    value: "create",
+    action: () => {
+      console.log("Création d'un compte");
+    },
+  },
+]);
+
+cli.menu();
